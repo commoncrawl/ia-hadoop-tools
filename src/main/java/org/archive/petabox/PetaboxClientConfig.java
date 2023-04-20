da349bddee897a9a028f3b7e7c01c8cb82cb6856
Sebastian Nagel <sebastian@commoncrawl.org>
2023-04-20 14:50:53 +0200

    Remove unneeded classes and their dependencies - Pig, Cassandra, Petabox, server - in order to address #4 (failed to download dependencies   from builds.archive.org)

--- src/main/java/org/archive/petabox/PetaboxClientConfig.java
+++
@@ -1,7 +0,0 @@
-package org.archive.petabox;
-
-public interface PetaboxClientConfig {
-	public int getInt(String name, int defaultValue);
-	public boolean getBoolean(String name, boolean defaultValue);
-	public String getString(String name);
-}
